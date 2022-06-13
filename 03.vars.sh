e6f710131166b67c6bf1772766a3d86a495877f1
nagalakshmi <chinnak7416@gmail.com>
2022-06-13 19:58:47 +0530

    Variable topic

--- 03.vars.sh
+++ 03.vars.sh
@@ -15,3 +15,6 @@ echo Good evening, Today date is $DATE
 ## Arthmetic Substitution
 EXPR1=$((2+3-4*5/7*8))
 echo EXPR1 OUTPUT = $EXPR1
+
+## Trying to access a variable from shell command line
+echo Course Name= $COURSE Name
